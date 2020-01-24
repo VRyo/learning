e81e6889b0b3cb836756eb0e847d503d6ec8378e
Fajrul Aslim <30421357+fajrulaslim@users.noreply.github.com>
2020-01-24 10:54:40 +0700

    [fajrul]
    update folder bootstrap
    update folder inverview

---
+++ php_basic/strtotime.php
@@ -0,0 +1,11 @@
+<?php 
+	$haha = '05 Oct 2019';
+	echo strtotime($haha);
+	$date = 1576497600;
+	echo '<br>';
+	echo date('d F Y', $date);
+	echo '<br>';
+	echo 'haha';
+	echo strtotime('');
+
+ ?>
